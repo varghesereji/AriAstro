e16fda2b48793e2c6c2ec737c3176e352adaa0d9
Varghese <varghesereji0007@gmail.com>
2025-09-02 15:59:08 +0530

    Initial version of code

---
+++ src/ariastro/main.py
@@ -0,0 +1,138 @@
+from pathlib import Path
+from astropy.io import fits
+import numpy as np
+
+from collections import defaultdict
+
+
+def create_fits(datadict, header_dict, filename="Avg_neid_data.fits"):
+    header_names = list(datadict.keys())
+    
+    hdus = []
+
+    # --- Primary HDU ---
+    primary_data = np.atleast_1d(datadict[header_names[0]])
+    if primary_data.ndim == 1:
+        primary_data = primary_data.reshape(1, -1)  # ensure 2D
+    primary_header = fits.Header(header_dict[header_names[0]])
+    primary_hdu = fits.PrimaryHDU(primary_data, header=primary_header)
+    hdus.append(primary_hdu)
+
+    # --- Extensions ---
+    for exts in header_names[1:]:
+        data = np.atleast_1d(datadict[exts])
+        try:
+            if data.ndim == 1:
+                data = data.reshape(1, -1)  # ensure 2D
+            ext_header = fits.Header(header_dict[exts])
+            hdu = fits.ImageHDU(data, header=ext_header, name=exts)
+        except KeyError:
+            data = np.array([np.nan, np.nan])
+            ext_header = fits.Header(header_dict[exts])
+            hdu = fits.ImageHDU(data, header=ext_header, name=exts)
+        hdus.append(hdu)
+
+    # --- Write FITS ---
+    hdul = fits.HDUList(hdus)
+    hdul.writeto(filename, overwrite=True)
+
+
+def get_data(hdu, fluxext, wlext, varext):
+    '''
+    Extracting data from hdu
+    '''
+    datadict = {'flux': np.array(hdu[fluxext].data).astype(np.float64),
+                'wl': np.array(hdu[wlext].data).astype(np.float64)}
+    if varext is not None:
+        datadict['var'] = np.array(hdu[varext].data).astype(np.float64)
+    return datadict
+
+
+def combine_data(datadict, method='mean', skipexts=[0, 13]):
+    dictkeys = list(datadict.keys())
+    comb_dicts = {}
+    # print(dictkeys)
+    for n, keys in enumerate(dictkeys):
+        print(n, keys, skipexts)
+        data = datadict[keys]
+        if n in skipexts:
+            comb_data = data[0]
+        else:
+            if method == 'mean':
+                comb_data = np.nanmean(data, axis=0)
+            elif method == 'median':
+                comb_data = np.nanmedian(data, axis=0)
+        comb_dicts[keys] = comb_data
+    return comb_dicts
+
+
+def combine_spectra(filesre="*.fits", directory=".",
+                    fluxext=(1, 2, 3),
+                    varext=(4, 5, 6),
+                    wlext=(7, 8, 9),
+                    orders=(173, 52)):
+    '''
+    Function to combine spectra.
+    Input
+    -------
+    filesre: Regular expression for the files.
+    directory: data directory.
+    fluxext: extension for flux array.
+
+    '''
+    print(filesre)
+    if isinstance(filesre, list):
+        files_list = filesre
+    elif isinstance(filesre, str):
+        files_path = Path(directory)
+        files_list = files_path.glob(filesre)
+    else:
+        print("Enter either files list or the regular expression")
+        return
+    
+    data_dict = defaultdict(list)
+    header_dict = {}
+    flag = 0
+    file_list = []
+    for specfile in files_list:
+        print(specfile)
+        file_list.append(specfile.name)
+        hdulist = fits.open(specfile)
+        for i, hdu in enumerate(hdulist):
+            extname = hdu.header.get('EXTNAME', f'HDU{i}')
+            # print(i, extname)
+            try:
+                data = np.array(hdulist[i].data).astype(np.float64)
+            except TypeError:
+                # print('TypeError')
+                data = np.array(hdulist[i].data)
+            data_dict[extname].append(data)
+            if flag == 0:
+                header_dict[extname] = hdulist[i].header
+        flag += 1
+        # for e, exts in enumerate(fluxext):
+        #     print("Ext:", exts)
+        #     datadict = get_data(hdu, fluxext[e], varext[e], wlext[e])
+        #     if wl_master is None:
+        #         wl_master = datadict['wl']
+        #         flux_array = datadict['flux']
+        #         var_array = datadict['var']
+        #     else:
+        #         flux_array = np.vstack((flux_array, datadict['flux']))
+        #         var_array = np.vstack((var_array, datadict['var']))
+  
+        hdulist.close()
+    combined_dict = combine_data(data_dict)
+    # print(combined_dict)
+    print(file_list)
+    header_dict['HDU0']['HISTORY'] = "Combined {}".format(list(file_list))
+    create_fits(combined_dict, header_dict)
+    # print(header_dict)
+    del data_dict
+    # print(np.array(flux).shape)
+        
+if __name__ == '__main__':
+    path = '/home/varghese/Desktop/test_arastro'
+    filesre = "*T16*fits"
+    # filesre = list(Path(path).glob('*.fits'))
+    combine_spectra(filesre, path)
